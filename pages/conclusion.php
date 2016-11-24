1f7ec7aafdb698bb0e1c9d031eafd82eacb03fe4
Melanie Hum <m7hum@scs.ryerson.ca>
2016-11-24 13:55:54 -0500

    added more bullshit

--- pages/conclusion.php
+++ pages/conclusion.php
@@ -4,8 +4,17 @@ $content = <<<EOF
 <h1>Conclusion</h1>
 
 <p>
-This is over.
+Our overall experience with Laravel was "okay." For what we wanted to create, the framework did perform to our expectations.<br><br>
+
+We all agree, however, that it was not worth all the effort that went into trying to install and host a server for it.<br><br>
+
+Pages were fairly easy to create as Laravel is a mix of HTML and PHP. A noteable difference is that Laravel uses curly brackets { } to show when PHP is being used whereas HTML uses angle brackets and question marks, <xmp> <? ?> </xmp> to show when PHP is being used.<br><br>
+
+There is definitely a steep learning curve for Laravel compared to regular HTML and CSS. Laravel has many features that allow the user to create databases and implement secutrity (authentication, authorization, etc); however, these features were not neccessary for us to complete the assignemnt so we did not use them.<br><br>
+
+Finally, is there anything we would have done differently for this assignment? No, not really. But did we regret choosing Laravel? Yeah, probably.
 </p>
+
 EOF;
 
 include '../layouts/default.php';
