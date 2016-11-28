d26699508de069994fc3cf9826b6e5627018424f
melanieHum <melanie.hum@ryerson.ca>
2016-11-28 01:55:22 -0500

    Edited format. Subject to change.
    I don't know what it looks like yet, so yeah. I might change the <h3> tags to <b><i> tags instead if the <h3>tags don't look good.

--- pages/conclusion.php
+++ pages/conclusion.php
@@ -3,17 +3,23 @@
 $content = <<<EOF
 <h1>Conclusion</h1>
 
-<p>
-Our overall experience with Laravel was "okay." For what we wanted to create, the framework did perform to our expectations.<br><br>
+<h3>Did the framework/CMS you choose perform to your expectations?</h3>
+<p>For what we wanted to create, Laravel performed to our expectations.</p>
 
-We all agree, however, that it was not worth all the effort that went into trying to install and host a server for it.<br><br>
+<h3>Was the framework/CMS you chose difficult to install or configure?</h3>
+<p>Installing Laravel and trying to host a server for it was a pain in the butt.</p>
 
-Pages were fairly easy to create as Laravel is a mix of HTML and PHP. A noteable difference is that Laravel uses curly brackets { } to show when PHP is being used whereas HTML uses angle brackets and question marks, <xmp> <? ?> </xmp> to show when PHP is being used.<br><br>
+<h3>Was it easy the create the page with the chosen framework/CMS?</h3>
+<p>Pages were fairly easy to create, as Laravel is a mix of HTML and PHP. A noteable difference is that Laravel uses curly brackets { } to show when PHP is being used whereas HTML uses angle brackets and question marks, <xmp> <? ?> </xmp> to show when PHP is being used.</p>
 
-There is definitely a steep learning curve for Laravel compared to regular HTML and CSS. Laravel has many features that allow the user to create databases and implement secutrity (authentication, authorization, etc); however, these features were not neccessary for us to complete the assignemnt so we did not use them.<br><br>
+<h3>Was the learning curve steep compared with regular HTML/CSS?</h3>
+<p>There is definitely a steep learning curve for Laravel compared to regular HTML and CSS. Laravel has many features that allow the user to create databases and implement secutrity (authentication, authorization, etc); however, these features were not neccessary for us to complete the assignemnt so we did not use them.</p>
 
-Finally, is there anything we would have done differently for this assignment? No, not really. But did we regret choosing Laravel? Yeah, probably.
-</p>
+<h3>Would you have done things differently in retrospect?</h3>
+<p>Not really...</p>
+
+<h3>Did you regret your choice of framework/CMS?</h3>
+<p>Haha...Yeah.</p>
 
 EOF;
 
